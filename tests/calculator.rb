16097b69e896111543a93d79b7acfa0bde25e03a
waikyjung <waiky.jung@gmail.com>
2024-10-12 19:25:30 +0000

    minitest

---
+++ tests/calculator.rb
@@ -0,0 +1,9 @@
+require "minitest/autorun"
+require "./calculator"
+
+class TestCalculator < Minitest:: Test
+  def test_addition
+    calculator = Calculator.new
+    assert_equal 4, calculator.add(2, 2). "Addition method failed"
+  end
+end
